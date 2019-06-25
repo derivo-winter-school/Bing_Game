ab0fc1b70520e05501dcb07b25108c5a89a3d931
Yashin <ysoobramoney@gmail.com>
2019-06-25 13:51:44 +0200

    Revert "ArrayComplete"
    This reverts commit be3ba6c3601632b43e796db22808659228d3f6b5.

---
+++ Bingo_Game_Mark_1/Classes/Randomize.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bingo_Game_Mark_1.Classes
+{
+    public class Randomize
+    {
+
+        public int Random()
+        {
+                Random R = new Random();
+                int num = R.Next(1, 91);
+            return num;
+
+          
+        }
+
+
+    }
+}
---
+++ Bingo_Game_Mark_1/RandomDisplay.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Bingo_Game_Mark_1.Classes;
+
+namespace Bingo_Game_Mark_1
+{
+    public partial class RandomDisplay : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        } 
+
+        protected void display_Click(object sender, EventArgs e)
+        {
+            Randomize R = new Randomize();
+            lbldisplay.Text = R.Random().ToString();
+            txtText.Text.ToString();
+
+            if(lbldisplay.Text == txtText.Text.ToString())
+            {
+                lblAnswer.Text = "You Do Not Have Bingo";
+            }
+            
+            
+       
+        }
+
+        protected void DisplayArray_Click(object sender, EventArgs e)
+        {
+           string[] lines = File.ReadAllLines("C:/Users/student/Desktop/Bingo_Game_Mark_1/Bingo_Game_Mark_1/TextFile/bingocard5.txt");
+            TextBox1.Text = "";
+            
+            foreach (string line in lines)
+            {
+                string[] arr = line.Split(' ');
+                for (int x = 0;x<9;x++)
+                {
+                    TextBox1.Text += arr[x]+" ";
+                    
+                }
+
+                TextBox1.Text += "\n";
+            }
+
+        }
+    }
+}
